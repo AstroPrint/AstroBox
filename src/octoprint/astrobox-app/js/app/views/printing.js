7c0d770b49622b832a2e85611a7a9b8f565e5611
Daniel Arroyo <darroyo@chill.com>
2014-05-18 20:12:44 -0400

    prettier printing screen

--- src/octoprint/astrobox-app/js/app/views/printing.js
+++ src/octoprint/astrobox-app/js/app/views/printing.js
@@ -17,19 +17,33 @@
     },
     onTempsChanged: function(s, value) {
         if (!this.$el.hasClass('hide')) {
-            this.$el.find('#printing-bed-target').text(value.bed.target);
-            this.$el.find('#printing-bed-current').text(Math.round(value.bed.actual));
-            this.$el.find('#printing-nozzle-target').text(value.extruder.target);
-            this.$el.find('#printing-nozzle-current').text(Math.round(value.extruder.actual));
+            this.$el.find('.temperatures .nozzle .temp-target').text(value.extruder.target);
+            this.$el.find('.temperatures .nozzle .temp-current').html(Math.round(value.extruder.actual)+'&deg;');
+            this.$el.find('.temperatures .bed .temp-target').text(value.bed.target);
+            this.$el.find('.temperatures .bed .temp-current').html(Math.round(value.bed.actual)+'&deg;');
         }
     },
     onProgressChanged: function(s, value) {
+        //progress bar
         this.$el.find('.progress .meter').css('width', value.percent+'%');
-        this.$el.find('#printing-time').text(this._formatTime(value.time_left));
-        this.$el.find('#printing-passed').text(this._formatTime(value.time_elapsed));
-        this.$el.find('#current-layer').text(value.current_layer);
-        this.$el.find('#layer-count').text(value.layer_count);
-        this.$el.find('#heating-up').text(value.heating_up ? 'YES' : 'NO');
+        this.$el.find('.progress .progress-label').text(Math.round(value.percent)+'%');
+
+        //time
+        var time = this._formatTime(value.time_left);
+        this.$el.find('.estimated-hours').text(time[0]);
+        this.$el.find('.estimated-minutes').text(time[1]);
+        this.$el.find('.estimated-seconds').text(time[2]);
+
+        //layers
+        this.$el.find('.current-layer').text(value.current_layer);
+        this.$el.find('.layer-count').text(value.layer_count);
+
+        //heating up
+        if (value.heating_up) {
+            this.$el.find('.print-info').addClass("heating-up");
+        } else {
+            this.$el.find('.print-info').removeClass("heating-up");
+        }
     },
     _formatTime: function(seconds) {
         var sec_num = parseInt(seconds, 10); // don't forget the second param
@@ -40,18 +54,20 @@
         if (hours   < 10) {hours   = "0"+hours;}
         if (minutes < 10) {minutes = "0"+minutes;}
         if (seconds < 10) {seconds = "0"+seconds;}
-        return hours+':'+minutes+':'+seconds;
+        return [hours, minutes, seconds];
     },
     onPausedChanged: function(s, value) {
         var pauseBtn = this.$el.find('button.pause-print')
 
         if (value) {
-            pauseBtn.text('RESUME PRINT');
+            pauseBtn.html('<i class="icon-play"></i> Resume Print');
         } else {
-            pauseBtn.text('PAUSE PRINT');
+            pauseBtn.html('<i class="icon-pause"></i> Pause Print');
         }
     },
 	startPrint: function(filename, cb) {
+        var self = this;
+
         $.ajax({
             url: '/api/files/local/'+filename,
             type: "POST",
@@ -60,6 +76,7 @@
             data: JSON.stringify({command: "select", print: true})
         }).
         done(function() {
+            self.$el.find('.progress .filename').text(filename);
         	cb(true);
         }).
         fail(function() {
