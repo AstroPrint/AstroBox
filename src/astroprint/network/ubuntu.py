6fce5e2967e45abb4a8b8dd5ef174c52a4ba8ef8
Daniel Arroyo <darroyo@chill.com>
2014-08-20 16:12:53 -0700

    fix: avoid resetting hotspot name on upgrade

--- src/astroprint/network/ubuntu.py
+++ src/astroprint/network/ubuntu.py
@@ -21,22 +21,6 @@ from tempfile import mkstemp
 from shutil import move
 from os import remove, close
 
-def replace(file_path, pattern, subst):
-    #Create temp file
-    fh, abs_path = mkstemp()
-    new_file = open(abs_path,'w')
-    old_file = open(file_path)
-    for line in old_file:
-        new_file.write(line.replace(pattern, subst))
-    #close temp file
-    new_file.close()
-    close(fh)
-    old_file.close()
-    #Remove original file
-    remove(file_path)
-    #Move new file
-    move(abs_path, file_path)
-
 class UbuntuNetworkManager(NetworkManagerBase):
 	def getWifiNetworks(self):
 		interface = self.settings.get(['wifi', 'internetInterface'])
@@ -183,9 +167,24 @@ class UbuntuNetworkManager(NetworkManagerBase):
 		settings.SaveHostname(name)
 
 		if (settings.Hostname == name):
+			def replace(file_path, pattern, subst):
+				#Create temp file
+				fh, abs_path = mkstemp()
+				new_file = open(abs_path,'w')
+				old_file = open(file_path)
+				for line in old_file:
+					new_file.write(line.replace(pattern, subst))
+				#close temp file
+				new_file.close()
+				close(fh)
+				old_file.close()
+				#Remove original file
+				remove(file_path)
+				#Move new file
+				move(abs_path, file_path)
+			
 			udpateFiles = [
-				'/etc/hosts', 
-				'/etc/hostapd/hostapd.conf'
+				'/etc/hosts'
 			]
 
 			for f in udpateFiles:
