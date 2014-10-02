f06ae4ebc6b0ddd33a90bc025cf228e98b633cc1
Daniel Arroyo <daniel.arroyo@gmail.com>
2014-10-02 04:16:42 +0000

    taking pic

--- src/astroprint/camera/__init__.py
+++ src/astroprint/camera/__init__.py
@@ -2,6 +2,8 @@
 __author__ = "Daniel Arroyo <daniel@3dagogo.com>"
 __license__ = 'GNU Affero General Public License http://www.gnu.org/licenses/agpl.html'
 
+from sys import platform
+
 # singleton
 _instance = None
 
@@ -9,7 +11,7 @@ def cameraManager():
 	global _instance
 	if _instance is None:
 		if platform == "linux" or platform == "linux2":
-			from astroprint.cameara.video4linux import CameraV4LManager
+			from astroprint.camera.video4linux import CameraV4LManager
 			_instance = CameraV4LManager()
 		else:
 			_instance = CameraManager()
@@ -18,7 +20,10 @@ def cameraManager():
 
 class CameraManager(object):
 	def __init__(self):
-		print self.list_cameras()
+		pass
 
 	def list_cameras(self):
+		pass
+
+	def save_pic(self):
 		pass
--- src/astroprint/camera/video4linux.py
+++ src/astroprint/camera/video4linux.py
@@ -2,6 +2,8 @@
 __author__ = "Daniel Arroyo <daniel@3dagogo.com>"
 __license__ = 'GNU Affero General Public License http://www.gnu.org/licenses/agpl.html'
 
+import cv2
+
 from os import listdir
 
 from astroprint.camera import CameraManager
@@ -9,3 +11,17 @@ from astroprint.camera import CameraManager
 class CameraV4LManager(CameraManager):
 	def list_cameras(self):
 		return listdir('/sys/class/video4linux')
+
+	def save_pic(self):
+		camera = cv2.VideoCapture(0)
+		for i in range(30):
+			camera.read()
+
+		s, img = camera.read()
+		if s:
+			cv2.imwrite('/sdcard/development/AstroBox/src/astroprint/static/img/text.jpeg', img)
+		else:
+			print "Error getting image"
+
+		del(camera)
+
