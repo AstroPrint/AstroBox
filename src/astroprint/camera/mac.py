a04cc15b9db8d0f4b4bc54b180dae45a81dde5ab
Daniel Arroyo <darroyo@chill.com>
2014-10-13 20:45:39 -0700

    random pick from a set of files in mac emulation

--- src/astroprint/camera/mac.py
+++ src/astroprint/camera/mac.py
@@ -3,16 +3,25 @@ __author__ = "Daniel Arroyo <daniel@3dagogo.com>"
 __license__ = 'GNU Affero General Public License http://www.gnu.org/licenses/agpl.html'
 
 import os.path
+import glob
 
+from random import randrange
 from astroprint.camera import CameraManager
 
 class CameraMacManager(CameraManager):
+	def __init__(self):
+		super(CameraMacManager, self).__init__()
+
+		self._files = [f for f in glob.glob(os.path.join(os.path.realpath(os.path.dirname(__file__)+'/../../../local'),"camera_test*.jpeg"))]
+
 	def open_camera(self):
 		return True
 
 	def get_pic(self, text=None):
-		imageFile = os.path.join(os.path.realpath(os.path.dirname(__file__)+'/../../../local'), "camera_test.jpeg")
-		if os.path.isfile(imageFile):
+		fileCount = len(self._files)
+
+		if fileCount:
+			imageFile = self._files[randrange(fileCount)]
 			with open(imageFile, "r") as f:
 				image = f.read()
 
