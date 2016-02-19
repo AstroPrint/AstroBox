396cf20a07bde7b157f2fae148e05ae344565d8a
Daniel Arroyo <daniel.arroyo@gmail.com>
2016-02-19 12:04:22 +0100

    remove unused library

--- src/astroprint/webrtc/__init__.py
+++ src/astroprint/webrtc/__init__.py
@@ -1,5 +1,4 @@
 # coding=utf-8
-from gst.video import VideoSink
 __author__ = "Daniel Arroyo <daniel@astroprint.com>"
 __license__ = 'GNU Affero General Public License http://www.gnu.org/licenses/agpl.html'
 
