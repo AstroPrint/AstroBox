72fc226b09987819f56d7fc964e8039a392c23b1
Rafael Luque <rafael@astroprint.com>
2016-06-17 14:44:15 +0000

    starting video with default fps error

--- src/octoprint/settings.py
+++ src/octoprint/settings.py
@@ -50,7 +50,7 @@ default_settings = {
 		"manager": "gstreamer",
 		"encoding": "h264",
 		"size": "640x480",
-		"framerate": "1/15",
+		"framerate": "15/1",
 		"format": "x-raw",
 		"pixelformat": "YUYV"
 	},
