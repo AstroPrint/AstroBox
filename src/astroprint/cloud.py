520b79f2e6276a69da990a74aaa7682fff78b8f0
Sonia <sonia.cejas.soria@gmail.com>
2017-04-26 13:27:04 +0200

    update print_file_store when download a new print file

--- src/astroprint/cloud.py
+++ src/astroprint/cloud.py
@@ -77,7 +77,7 @@ class AstroPrintCloud(object):
 				self.hmacAuth = HMACAuth(user.publicKey, user.privateKey)
 
 		self.apiHost = self.settings.get(['cloudSlicer', 'apiHost'])
-		self._print_file_store = []
+		self._print_file_store = None
 		self._sm = softwareManager()
 		self._logger = logging.getLogger(__name__)
 
@@ -268,7 +268,7 @@ class AstroPrintCloud(object):
 		return data
 
 	def print_files(self, forceCloudSync = False):
-		if self.cloud_enabled() and (not self._print_file_store or forceCloudSync):
+		if self.cloud_enabled() and (self._print_file_store is None or forceCloudSync):
 			self._sync_print_file_store()
 
 		return json.dumps(self._print_file_store)
@@ -276,13 +276,13 @@ class AstroPrintCloud(object):
 	def download_print_file(self, print_file_id, progressCb, successCb, errorCb):
 		fileManager = printerManager().fileManager
 
-		printFile = fileManager.getFileByCloudId(print_file_id)
-
 		try:
 			r = requests.get('%s/print-files/%s' % (self.apiHost, print_file_id), auth=self.hmacAuth)
 			data = r.json()
 		except:
 			data = None
+
+		printFile = fileManager.getFileByCloudId(print_file_id)
 
 		if printFile:
 			self._logger.info('Print file %s is already on the box as %s' % (print_file_id, printFile))
@@ -293,6 +293,11 @@ class AstroPrintCloud(object):
 
 				if data["printFileName"] != localPrintFileName:
 					pm.fileManager.setPrintFileName(printFile, data["printFileName"])
+					#update printFileName for this printFile in the collection
+					for x in self._print_file_store:
+						if x['id'] == print_file_id:
+							x['printFileName'] = data["printFileName"]
+							break
 
 			successCb(printFile, True)
 			return True
@@ -317,6 +322,10 @@ class AstroPrintCloud(object):
 			destFile = fileManager.getAbsolutePath(data['name'], mustExist=False)
 
 			if destFile:
+				def onSuccess(pf, error):
+					self._print_file_store = None
+					successCb(pf, error)
+
 				downloadManager().startDownload({
 					'downloadUrl': data["download_url"],
 					'destFile': destFile,
@@ -324,7 +333,7 @@ class AstroPrintCloud(object):
 					'printFileInfo': data['info'],
 					'printFileName': printFileName,
 					'progressCb': progressCb,
-					'successCb': successCb,
+					'successCb': onSuccess,
 					'errorCb': errorCb
 				})
 
