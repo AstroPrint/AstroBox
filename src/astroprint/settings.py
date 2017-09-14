f2e47adbaa0c5d0a2a92ec84b93ac5ed404b5c13
Alberto Mora <moracabanas@me.com>
2017-09-15 01:36:13 +0200

    Deleted originals
    Fresh start, to make an installation file

--- src/astroprint/settings.py
+++
@@ -1,7 +0,0 @@
-# coding=utf-8
-__author__ = "AstroPrint Product Team <product@astroprint.com>"
-__license__ = "GNU Affero General Public License http://www.gnu.org/licenses/agpl.html"
-__copyright__ = "Copyright (C) 2016 3DaGoGo, Inc - Released under terms of the AGPLv3 License"
-
-ASSETS_DEBUG = True
-ASSETS_AUTO_BUILD = True
--- src/ext/__init__.py
+++
@@ -1,3 +0,0 @@
-# coding=utf-8
-__author__ = "AstroPrint Product Team <product@astroprint.com>"
-__license__ = 'GNU Affero General Public License http://www.gnu.org/licenses/agpl.html'
