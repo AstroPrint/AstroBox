f912fadcaaaf2109e792045c847455769b7e3f4c
rafa <rafael@astroprint.com>
2017-10-25 14:26:15 +0200

    cleaning source code

--- src/astroprint/plugin/services/printer.py
+++ src/astroprint/plugin/services/printer.py
@@ -76,69 +76,46 @@ class PrinterService(PluginService):
 		return {}
 
 	def printerPrintheadCommand(self, data, callback):
-		self._logger.info('printerPrintheadCommand')
-		self._logger.info(data)
-		self._logger.info(callback)
-		self._logger.info('-----------------------')
 		pm = printerManager()
-
-		command = 'jog'
-		axis = data
 
 		if not pm.isOperational() or pm.isPrinting():
 			# do not jog when a print job is running or we don't have a connection
 			callback("Printer is not operational or currently printing",True)
 
-		valid_commands = {
-			"jog": [],
-			"home": ["axes"]
-		}
-
 		valid_axes = ["x", "y", "z"]
-		##~~ jog command
-		if command == "jog":
-			# validate all jog instructions, make sure that the values are numbers
 
-			self._logger.info('jog')
-			validated_values = {}
-			for axis in valid_axes:
-				self._logger.info('axis')
-				self._logger.info(axis)
-				try:
-					value = data[axis]
-				except:
-					value = None
-				self._logger.info('value')
-				self._logger.info(value)
-				if not isinstance(value,(int,long,float)):
-					self._logger.info('not a number')
-				else:
-					validated_values[axis] = value
-					self._logger.info('value added')
+		validated_values = {}
+		for axis in valid_axes:
+			try:
+				value = data[axis]
+			except:
+				value = None
+			if not isinstance(value,(int,long,float)):
+				self._logger.error('not a number')
+				callback('movement value is not a number',True)
+			else:
+				validated_values[axis] = value
 
-			self._logger.info('before ex')
-			self._logger.info(validated_values['x'])
-
-			# execute the jog commands
-			for axis, value in validated_values.iteritems():
-				self._logger.info('pm')
-				self._logger.info('axis %s',axis)
-				self._logger.info('value &s',value)
-				pm.jog(axis, value)
-
-		##~~ home command
-		elif command == "home":
-			validated_values = []
-			axes = axis
-			for axis in axes:
-				if not axis in valid_axes:
-					callback("Invalid axis: " + axis,True)
-				validated_values.append(axis)
-
-			# execute the home command
-			pm.home(validated_values)
+		# execute the jog commands
+		for axis, value in validated_values.iteritems():
+			pm.jog(axis, value)
 
 		callback({'success': 'no_error'})
+
+
+	def homeCommand(self,axes):
+
+		valid_axes = ["x", "y", "z"]
+
+		validated_values = []
+
+		for axis in axes:
+			if not axis in valid_axes:
+				callback("Invalid axis: " + axis,True)
+			validated_values.append(axis)
+
+		# execute the home command
+		pm.home(validated_values)
 
 
 	#EVENTS
