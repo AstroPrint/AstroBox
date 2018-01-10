c2eae35bc3d10a835b83dac679242cd88fc9b57d
alfonsopl <alfonsoplgu@gmail.com>
2018-01-10 10:17:47 +0100

    unmatch variable name fixed

--- src/astroprint/plugin/services/account.py
+++ src/astroprint/plugin/services/account.py
@@ -111,12 +111,12 @@ class AccountService(PluginService):
 	#EVENTS
 
 	def _onAccountStateChange(self,event,value):
-			sets = settings()
+			asets = settings()
 			data = {
 				'state':  "connected" if  sets.get(["cloudSlicer", "loggedUser"]) else "disconnected",
 				'boxrouterStatus' :  boxrouterManager().status
 			}
 			if sets.get(["cloudSlicer", "loggedUser"]):
-				payload['user'] = sets.get(["cloudSlicer", "loggedUser"])
+				data['user'] = sets.get(["cloudSlicer", "loggedUser"])
 
 			self.publishEvent('account_state_change',data)
