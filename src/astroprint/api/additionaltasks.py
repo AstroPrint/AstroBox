438d241da85b5620cf0902691794dac78be5e815
Daniel Arroyo <daniel.arroyo@gmail.com>
2018-04-20 15:49:08 +0200

    upload & process task zip

--- src/astroprint/api/additionaltasks.py
+++ src/astroprint/api/additionaltasks.py
@@ -2,7 +2,7 @@
 __author__ = "Daniel Arroyo <daniel@astroprint.com>"
 __license__ = 'GNU Affero General Public License http://www.gnu.org/licenses/agpl.html'
 
-from flask import jsonify
+from flask import jsonify, request, make_response
 
 from octoprint.server.api import api
 from octoprint.server import restricted_access
@@ -16,3 +16,33 @@ def additionalTasks():
 
 	result = atm.data.copy()
 	return jsonify( result )
+
+@api.route('/additional-tasks', methods=['POST'])
+@restricted_access
+def additionalTasksCreate():
+	if not "file" in request.files.keys():
+		return make_response("No file included", 400)
+
+	file = request.files["file"]
+
+	atm = additionalTasksManager()
+	r = atm.checkTaskFile(file)
+
+	if 'error' in r:
+		return make_response(r['error'], 500)
+	else:
+		return jsonify(r)
+
+@api.route("/additional-tasks/install", methods=["POST"])
+@restricted_access
+def installTask():
+	data = request.json
+	file = data.get('file', None)
+	if file:
+		atm = additionalTasksManager()
+		if atm.installFile(file):
+			return jsonify()
+		else:
+			return make_response('Unable to Install', 500)
+
+	return make_response('Invalid Request', 400)
--- src/astroprint/static/js/app/views/additional-tasks.js
+++ src/astroprint/static/js/app/views/additional-tasks.js
@@ -1,12 +1,23 @@
 var AdditionalTasksView = Backbone.View.extend({
   el: '#additional-tasks-view',
   additionalTasksListView: null,
+  upload: null,
   initialize: function()
   {
     this.additionalTasksListView = new AdditionalTasksListView();
+    this.uploader = new TaskUploader({
+      el: this.$('input.file-upload'),
+      progressBar: this.$('.upload-progress'),
+      buttonContainer: this.$('.upload-buttons'),
+      installedCallback: _.bind(this.onTaskInstalled,this)
+    });
+  },
+  onTaskInstalled: function()
+  {
+    noty({type: "success", text: "Task Installed", timeout: 3000});
+    this.additionalTasksListView.refreshList();
   }
 });
-
 
 // Custom actions menu
 var AdditionalTasksListView = Backbone.View.extend({
@@ -16,10 +27,12 @@ var AdditionalTasksListView = Backbone.View.extend({
   initialize: function()
   {
     this.additionalTaskCollection = new AdditionalTaskCollection();
-    //this.additionalTaskCollection = 'name'
+    this.refreshList();
+  },
+  refreshList: function()
+  {
+    this.additionalTaskCollection.reset();
     $.getJSON(API_BASEURL + 'additional-tasks', null, _.bind(function(data) {
-
-
       if (data.utilities && data.utilities.length) {
         for (var i = 0; i < data.utilities.length; i++) {
           var adTask = data.utilities[i];
@@ -36,6 +49,7 @@ var AdditionalTasksListView = Backbone.View.extend({
   },
   render: function()
   {
+    this.$el.empty();
     // Render each box
     this.additionalTaskCollection.each(function (additionalTaskApp) {
       var row = new AdditionalTaskRowView({ additionalTaskApp: additionalTaskApp});
@@ -63,6 +77,106 @@ var AdditionalTaskRowView = Backbone.View.extend({
   }
 });
 
+var TaskUploader = FileUploadBase.extend({
+  progressBar: null,
+  buttonContainer: null,
+  installedCallback: null,
+  initialize: function(options)
+  {
+    FileUploadBase.prototype.initialize.call(this, options);
 
+    this.progressBar = options.progressBar;
+    this.buttonContainer = options.buttonContainer;
+    this.$el.attr('accept', '.zip');
+    this.acceptFileTypes = /(\.|\/)(zip)$/i;
+    this.uploadUrl = API_BASEURL + 'additional-tasks';
+    this.installedCallback = options.installedCallback;
+  },
+  started: function(data)
+  {
+    if (data.files && data.files.length > 0) {
+      this.buttonContainer.hide();
+      this.progressBar.show();
+      FileUploadBase.prototype.started.call(this, data);
+    }
+  },
+  failed: function(error)
+  {
+    var message = null;
+    switch(error) {
+      case 'invalid_file':
+        message = 'The file is not a valid task';
+      break;
 
+      case 'invalid_task_file':
+        message = 'The task file has errors';
+      break;
 
+      case 'error_checking_file':
+        message = 'There was an error checking the task file';
+      break;
+
+      case 'invalid_tasks_definition':
+        message = 'The task definition file is not valid';
+      break;
+
+      case 'incompatible_task':
+        message = 'The API version used by the task is not compatible.';
+
+      case 'already_installed':
+        message = "The Task is already installed. Please remove old version first.";
+      break;
+    }
+
+    this.onError(message);
+  },
+  success: function(data)
+  {
+    if (data.result.tmp_file) {
+      $.ajax({
+        url: API_BASEURL + 'additional-tasks/install',
+        method: 'POST',
+        type: 'json',
+        contentType: 'application/json',
+        data: JSON.stringify({
+          file: data.result.tmp_file
+        })
+      })
+        .done(_.bind(function(){
+          this.onPrintFileUploaded();
+          this.installedCallback(data.result);
+        }, this))
+        .fail(_.bind(function(){
+          this.onError('Unable to install task');
+        }, this))
+    } else {
+      this.onError('Unable to install task');
+    }
+  },
+  progress: function(progress, message)
+  {
+    var intPercent = Math.round(progress);
+
+    this.progressBar.find('.meter').css('width', intPercent+'%');
+    if (!message) {
+      message = "Uploading ("+intPercent+"%)";
+    }
+    this.progressBar.find('.progress-message span').text(message);
+  },
+  onError: function(error)
+  {
+    noty({text: error ? error : 'There was an error uploading your file', timeout: 3000});
+    this.resetUploadArea();
+    console.error(error);
+  },
+  onPrintFileUploaded: function()
+  {
+    this.resetUploadArea();
+  },
+  resetUploadArea: function()
+  {
+    this.progressBar.hide();
+    this.buttonContainer.show();
+    this.progress(0);
+  }
+});
