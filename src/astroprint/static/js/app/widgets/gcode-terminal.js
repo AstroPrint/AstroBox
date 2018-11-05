e99322f2d229c07779b3f74252969d4f323e0616
julianCast <squeleton@gmail.com>
2018-11-05 13:43:21 +0100

    Gcode terminal is now a widget
    This is useful to use it in a task

---
+++ src/astroprint/static/js/app/widgets/gcode-terminal.js
@@ -0,0 +1,106 @@
+var GcodeWidgetView = Backbone.View.extend({
+  template: _.template( $("#gcode-terminal-template").html() ),
+  outputView: null,
+  ignoreReceived: ['wait'],
+  gcodeTerminalView: null,
+  events: {
+    'submit form': 'onSend',
+    'show': 'onShow',
+    'hide': 'onHide',
+    'click button.clear': 'onClear',
+    'click .alert-box.warning a': 'onDismissAlert'
+  },
+
+  initialize: function()
+  {
+    app.eventManager.on("astrobox:commsData", _.bind(function(data) {
+      if ( !(data.direction == 'r' && _.contains(this.ignoreReceived, data.data)) ) {
+        this.add(data.direction, data.data);
+      }
+    }, this));
+  },
+  render: function ()
+  {
+    return this.$el.html(this.template());
+  },
+  add: function(type, text, failed)
+  {
+    switch(type) {
+      case 's': // sent to printer
+        text = '<div class="sent bold'+(failed ? ' failed' : '')+'"><i class="icon-'+(failed ? 'attention' : 'angle-right')+'""></i>'+text+(failed ? ' <small>(failed to sent)</small>':'')+'</div>';
+      break;
+
+      case 'r': // received from printer
+        text = '<div class="received"><i class="icon-angle-left"></i>'+text+'</div>';
+      break;
+    }
+    this.$('.output-container').append(text);
+    this.$('.output-container').scrollTop(this.$el[0].scrollHeight);
+  },
+  clear: function()
+  {
+    this.$('.output-container').html('');
+  },
+  onClear: function(e)
+  {
+    e.preventDefault();
+
+    this.clear();
+  },
+  onSend: function(e)
+  {
+    e.preventDefault();
+    var sendField = this.$('input');
+    var command = sendField.val().toUpperCase();
+
+    if (command) {
+      var loadingBtn = this.$('button.send').closest('.loading-button');
+
+      loadingBtn.addClass('loading');
+
+      $.ajax({
+        url: API_BASEURL + 'printer/comm/send',
+        method: 'POST',
+        data: {
+          command: command
+        }
+      })
+        .fail(_.bind(function(){
+          loadingBtn.addClass('failed');
+          this.outputView.add('s', command, true);
+
+          setTimeout(function(){
+            loadingBtn.removeClass('failed');
+          }, 3000);
+        }, this))
+        .always(function(){
+          loadingBtn.removeClass('loading');
+          sendField.val('');
+          console.log('always');
+        });
+    }
+
+    return false;
+  },
+  onShow: function()
+  {
+    this.$('input').focus();
+    $.ajax({
+      url: API_BASEURL + 'printer/comm/listen',
+      method: 'POST'
+    })
+  },
+  onHide: function()
+  {
+    $.ajax({
+      url: API_BASEURL + 'printer/comm/listen',
+      method: 'DELETE'
+    });
+  },
+  onDismissAlert: function(e)
+  {
+    e.preventDefault();
+
+    this.$('.alert-box.warning').remove();
+  }
+});
