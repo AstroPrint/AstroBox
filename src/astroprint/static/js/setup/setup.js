37b48aaa394f41947daa6591cb23bef8603b0bc9
julianCast <squeleton@gmail.com>
2018-12-05 13:01:01 +0100

    fixed undefined error on setup template

--- src/astroprint/static/js/setup/setup.js
+++ src/astroprint/static/js/setup/setup.js
@@ -796,8 +796,8 @@ var StepPrinterSelection = StepView.extend({
   _checkCurrentPrinter: function() {
     var promise = $.Deferred();
 
-    if (MF_DEFINITION) {
-      this.astroprintApi.getModelInfo(MF_DEFINITION)
+    if (PRINTER_PROFILE_ID) {
+      this.astroprintApi.getModelInfo(PRINTER_PROFILE_ID)
       .done(_.bind(function (info) {
         this.printerInfo = info;
         this.$el.addClass('printer-selected');
