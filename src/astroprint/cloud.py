d661fbd576c24df5e1c24c7c14768a28f307c69c
Daniel Arroyo <daniel.arroyo@gmail.com>
2019-07-30 14:21:43 +0300

    update copyrights

--- src/astroprint/cloud.py
+++ src/astroprint/cloud.py
@@ -1,6 +1,6 @@
 __author__ = "AstroPrint Product Team <product@astroprint.com>"
 __license__ = "GNU Affero General Public License http://www.gnu.org/licenses/agpl.html"
-__copyright__ = "Copyright (C) 2016 3DaGoGo, Inc - Released under terms of the AGPLv3 License"
+__copyright__ = "Copyright (C) 2016-2019 3DaGoGo, Inc - Released under terms of the AGPLv3 License"
 
 # singleton
 _instance = None
