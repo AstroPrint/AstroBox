3a2d7c75caddbd94864680e23a8ee4e16a1a4bcd
lukemaster <rafael@astroprint.com>
2019-08-26 11:08:23 +0200

    fixes #589

--- src/astroprint/camera/local_video_handler.py
+++ src/astroprint/camera/local_video_handler.py
@@ -7,13 +7,12 @@ import datetime
 
 from astroprint.camera import cameraManager
 
-
 class VideoStreamHandler(tornado.web.RequestHandler):
-
-	def initialize(self):
+	def initialize(self,access_validation):
 		self._logger = logging.getLogger(__name__)
 		self.cameraMgr = cameraManager()
 		self.id = self.cameraMgr.addLocalPeerReq()
+		self._access_validation = access_validation
 
 	def on_finish(self):
 		self._logger.debug('on_finish id - %s' % self.id)
@@ -26,33 +25,39 @@ class VideoStreamHandler(tornado.web.RequestHandler):
 	@tornado.web.asynchronous
 	@tornado.gen.coroutine
 	def get(self):
+		try:
+			self._access_validation(self.request)
 
-		self.set_header('Cache-Control', 'no-store, no-cache, must-revalidate, pre-check=0, post-check=0, max-age=0')
-		self.set_header('Connection', 'close')
-		self.set_header('Content-Type', 'multipart/x-mixed-replace;boundary=--boundarydonotcross')
-		self.set_header('Pragma', 'no-cache')
-		self.set_header('Cache-Control', 'no-cache')
+			self.set_header('Cache-Control', 'no-store, no-cache, must-revalidate, pre-check=0, post-check=0, max-age=0')
+			self.set_header('Connection', 'close')
+			self.set_header('Content-Type', 'multipart/x-mixed-replace;boundary=--boundarydonotcross')
+			self.set_header('Pragma', 'no-cache')
+			self.set_header('Cache-Control', 'no-cache')
 
-		my_boundary = "--boundarydonotcross\n"
+			my_boundary = "--boundarydonotcross\n"
 
-		stillStreaming = True
+			stillStreaming = True
 
-		self.sendNextFrame = True
+			self.sendNextFrame = True
 
-		while stillStreaming:
+			while stillStreaming:
 
-			stillStreaming = self.cameraMgr.localSessionAlive(self.id)
+				stillStreaming = self.cameraMgr.localSessionAlive(self.id)
 
-			img = self.cameraMgr.getFrame(self.id)
+				img = self.cameraMgr.getFrame(self.id)
 
-			if img:
-				self.write(my_boundary)
-				self.write("Content-type: image/jpeg\r\n")
-				self.write("Content-length: %s\r\n\r\n" % len(img))
-				self.write(str(img))
-				yield tornado.gen.Task(self.flush)
-			else:
-				stillStreaming = None
+				if img:
+					self.write(my_boundary)
+					self.write("Content-type: image/jpeg\r\n")
+					self.write("Content-length: %s\r\n\r\n" % len(img))
+					self.write(str(img))
+					yield tornado.gen.Task(self.flush)
+				else:
+					stillStreaming = None
 
-		self.flush()
-		self.finish()
+			self.flush()
+			self.finish()
+
+		except Exception as e:
+			self._logger.error('local video streaming: %s' % e)
+			raise(e)
--- src/octoprint/server/__init__.py
+++ src/octoprint/server/__init__.py
@@ -518,7 +518,7 @@ class Server():
 			(r"/downloads/files/local/([^/]*\.(gco|gcode))", LargeResponseHandler, {"path": s.getBaseFolder("uploads"), "as_attachment": True}),
 			(r"/downloads/logs/([^/]*)", LargeResponseHandler, {"path": s.getBaseFolder("logs"), "as_attachment": True, "access_validation": access_validation_factory(admin_validator)}),
 			#(r"/downloads/camera/current", UrlForwardHandler, {"url": s.get(["webcam", "snapshot"]), "as_attachment": True, "access_validation": access_validation_factory(user_validator)}),
-			(r"/video-stream", VideoStreamHandler),
+			(r"/video-stream", VideoStreamHandler,{"access_validation": access_validation_factory(user_validator)}),
 			(r".*", FallbackHandler, {"fallback": WSGIContainer(app.wsgi_app)})
 		])
 		self._server = HTTPServer(self._tornado_app, max_buffer_size=1048576 * s.getInt(['server', 'maxUploadSize']))
